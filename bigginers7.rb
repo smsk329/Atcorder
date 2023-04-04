4aef95928febe857f8ce811a2bead55aa09b8204
EC2 Default User <smsk329@gmail.com>
2023-04-04 10:00:35 +0000

    [add]bigginers7

---
+++ bigginers7.rb
@@ -0,0 +1,20 @@
+# アリスのカードが大きくなるようにアリスとボブが交互にカードを引く
+# アリスはボブに何点差をつけて勝ったかを出力する
+
+n = gets.chomp.to_i
+i = gets.chomp.split(' ').map(&:to_i).sort.reverse
+alice = 0
+bob = 0
+
+# each_with_indexは配列に番号をふる。
+# 配列の最初の数字が０なので、偶数の場合にアリスがカードを引く
+i.each_with_index do |n, i|
+  if i.even?
+    alice = alice + n
+  else
+    bob = bob + n
+  end
+end
+
+puts alice - bob
+
